62472bb9945a519add6959a8af5084cb6b28f98a
Peter Smith <peter@upstateinteractive.io>
2019-11-10 22:07:47 -0500

    chore: add .env

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -9,5 +9,6 @@
  *
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/configurations/configurations.html#bootstrap
  */
+require('dotenv').config({ path: require('find-config')('.env') });
 
 module.exports = () => {};
